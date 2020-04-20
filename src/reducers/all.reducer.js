a4958cc106ca4228a1f24682d77b741aee782a1f
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-20 14:00:16 +0100

    Handle setCurrentLocation in sharedReducer

--- src/reducers/all.reducer.js
+++ src/reducers/all.reducer.js
@@ -1,5 +1,10 @@
+import { types } from "../actions/components";
+
 export const sharedReducer = (state={}, action) => {
   switch (action.type) {
+    case types.SET_CURRENT_LOCATION: {
+      return Object.assign({}, state, { currentLocation: action.location });
+    }
 
     default: {
       return state;
