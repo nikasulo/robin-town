e9c45bd11b82748f966fc2452100d1a78a5e607c
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-20 13:57:51 +0100

    Structure overlay and cta in hero section

--- src/components/hero/Hero.component.jsx
+++ src/components/hero/Hero.component.jsx
@@ -4,9 +4,12 @@ import './hero.styles.scss';
 const Hero = () => {
   return (
     <div className="hero column">
-      <div className="cta-holder row">
-        <i className="arrow-right-icon"></i>
-        <p>SHOP NOW</p>
+      <div className="overlay column">
+        <h2 className="hero-text">Sale of the summer collection</h2>
+        <div className="cta-holder row">
+          <i className="arrow-right-icon"></i>
+          <p className="cta-text">SHOP NOW</p>
+        </div>
       </div>
     </div>
   )
