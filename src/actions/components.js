94ca1dd70b7cd23b99ce532ff27541f5f80d05a6
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-21 11:08:00 +0100

    Add asos API headers to action axios config, add receive products to actions components

--- src/actions/components.js
+++ src/actions/components.js
@@ -1,3 +1,14 @@
+import Axios from "axios";
+
 export const types = {
   SET_CURRENT_LOCATION: "SET_CURRENT_LOCATION",
+  RECIEVE_PRODUCTS: "RECIEVE_PRODUCTS",
 };
+
+export const API = Axios.create({
+  baseURL: "https://asos2.p.rapidapi.com",
+  headers: {
+    "x-rapidapi-host": "asos2.p.rapidapi.com",
+    "x-rapidapi-key": "a1dd44e2ffmsh3aef00f95706593p14a95bjsnefeb4c14f217",
+  }
+});
