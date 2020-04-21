a85b0c60467623390e7f152eadfb4a2a0d87b067
oluwadamilareolusakin <oluwadamilare_olusakin@yahoo.com>
2020-04-21 11:09:53 +0100

    Write product actions

---
+++ src/actions/products.actions.js
@@ -0,0 +1,29 @@
+import { API, types, defaultHeaders } from "./components";
+
+const recieveProducts = (products, productType) => ({
+  type: types.RECIEVE_PRODUCTS,
+  productType,
+  products,
+})
+
+export const fetchProducts = (limit, type) => async(dispatch) => {
+  const parameters = {
+    "country": "US",
+    "currency": "USD",
+    "sort": "freshness",
+    "lang": "en-US",
+    "sizeSchema": "US",
+    "offset": "0",
+    "categoryId": "4209",
+    limit,
+    "store": "US"
+  }
+
+  try {
+    const { data } = await API.get('/products/v2/list', {params: parameters})
+    const { products } = data;
+    dispatch(recieveProducts(products, type));
+  } catch(error) {
+    console.log(error)
+  }
+};
